c6b1a0cf0d89adab6f1417578623edc713a2b5f8
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2025-04-13 16:26:31 +0200

    chore: cleanup

---
+++ src/features/layout/app-sidebar.tsx
@@ -0,0 +1,39 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+import {items} from '@/utils/sidebar-items'
+
+export default function AppSidebar() {
+  return (
+    <Sidebar collapsible='icon'>
+      <SidebarHeader></SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton tooltip={item.title} asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
